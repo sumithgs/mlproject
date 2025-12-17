43c3b9b5657f6572877582f9a46c1ba892e7d619
sumithgs <sumithgs2000@gmail.com>
2025-12-17 02:30:26 -0500

    Prediction pipeline

---
+++ app.py
@@ -0,0 +1,44 @@
+import pickle
+from flask import Flask, request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+app = Flask(__name__)
+
+# route for home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+    gender=request.form.get('gender', 'male'),
+    race_ethnicity=request.form.get('ethnicity', 'group A'),
+    parental_level_of_education=request.form.get(
+        'parental_level_of_education', 
+        'some college'
+    ),
+    lunch=request.form.get('lunch', 'standard'),
+    test_preparation_course=request.form.get(
+        'test_preparation_course', 
+        'none'
+    ),
+    reading_score=int(request.form.get('reading_score', 70)),
+    writing_score=int(request.form.get('writing_score', 70))
+)
+
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html',results=results[0])
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True,port=3001)
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,13 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
